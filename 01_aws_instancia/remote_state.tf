5622e2510a048bba10138ab43875c9ca76f8ff2f
soed8604 <ejsotelo4@gmail.com>
2023-04-13 14:35:41 -0500

    remote state locking

--- 01_aws_instancia/remote_state.tf
+++ 01_aws_instancia/remote_state.tf
@@ -3,5 +3,6 @@ terraform {
      bucket = "platzi-mi-repo1-para-terraform"
      key    = "terraform/states/01_aws_instancia.tfstate"
      region = "us-east-1"
+     dynamodb_table = "terraform-state-lock"
     } 
 }
